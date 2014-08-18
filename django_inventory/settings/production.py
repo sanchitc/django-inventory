dff03a9de89256549927e3df1deff643a3448db8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-08-18 01:19:27 -0400

    Use a production settings file with debug turned off

---
+++ django_inventory/settings/production.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from .local import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']  # Update this accordingly; https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+~                                                                                                                                                                               
+~                                                                                                                                                                               
+~                      
